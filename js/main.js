fa9a9e6908bafe960405d7eeb373a68ad633d495
Ku-Juheon <rnwngjs1015@gmail.com>
2024-05-04 23:29:38 +0900

    ch18 figure공통 스타일까지 완료

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 장바구니
 const basketStarterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket');
 
@@ -33,4 +34,65 @@ function showBasket() {
 }
 function hideBasket() {
   basketEl.classList.remove('show');
+}
+
+// 검색!
+const headerEl = document.querySelector('header');
+
+// ...은 전개연산자 headerEl.querySelectorAll('ul.menu > li')을 통해서 반환된 내용이 전개연산자를 통해서 해체됨 그리고 해체된 것이 []로 묶인다
+// 이렇게 배열로 하면 더 많은 함수를 사용할 수 있다
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')];
+
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const SearchShadowEl = searchWrapEl.querySelector('.shadow');
+const SearchInputEl = searchWrapEl.querySelector('input');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
+
+searchStarterEl.addEventListener('click', showSearching);
+searchCloserEl.addEventListener('click', hideSearching);
+SearchShadowEl.addEventListener('click', hideSearching);
+
+// searching이라는 함수로 검색 header에 searching 클래스 추가
+function showSearching() {
+  headerEl.classList.add('searching');
+  // html의 html요소 찾는 방법
+  document.documentElement.classList.add('fixed');
+  //console.log(headerMenuEls.reverse()); // -> 장바구니부터 사라지게 해야한다
+
+  headerMenuEls.reverse().forEach(function (el, index) {
+    // 딜레이가 0.4초 내에 진행되도록 할건데 그 안에 순차적으로 사라지게 할 것이다
+    //.length는 몇개가 알 수 있는지 함수 -> 나중에 ul태그가 추가되어도 자동으로 되게
+    // index가 0부터 시작하니까 11까지 나오게 되고 순차적으로 사라지게 한다
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + 's';
+  });
+
+  searchDelayEls.forEach(function (el, index) {
+    // search 안에 있는 li가 순차적으로 나타나게 하기
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + 's';
+  });
+
+  // 검색 input에 포커스 넣기 다 나타나고 난 뒤에 포커스 넣기
+  setTimeout(function () {
+    SearchInputEl.focus();
+  }, 600);
+}
+function hideSearching() {
+  headerEl.classList.remove('searching');
+  document.documentElement.classList.remove('fixed');
+
+  //showSearching에서 뒤집은 것을 다시 뒤집으니까 원래대로 순차적으로 적용이 된다
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + 's';
+  });
+
+  searchDelayEls.reverse().forEach(function (el, index) {
+    // 반대 순서로 사라지게 하기
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + 's';
+  });
+  // 이후에도 순차적으로 나타나도록 다시 뒤집기
+  searchDelayEls.reverse();
+
+  SearchInputEl.value = '';
 }
